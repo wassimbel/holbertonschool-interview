d5cd14a34a411b6dfd1b2f93202083963f1e01b9
wassimbel <wassim.belhedi1@gmail.com>
2021-06-03 22:43:58 +0000

    up

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def print_metrics(status_code, file_size):
+    """ reads stdin line by line and computes metrics
+        and prints file_size and status """
+
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+file_size = 0
+i = 0
+status_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+
+try:
+    for j in sys.stdin:
+        metrics = j.split()
+
+        if len(metrics) > 2:
+            i += 1
+            if i <= 10:
+                file_size += int(metrics[-1])
+                status = metrics[-2]
+
+                if (status in status_code.keys()):
+                    status_code[status] += 1
+
+            if (i == 10):
+                print_metrics(status_code, file_size)
+                i = 0
+
+except KeyboardInterrupt:
+    print_metrics(status_code, file_size)
+finally:
+    print_metrics(status_code, file_size)
