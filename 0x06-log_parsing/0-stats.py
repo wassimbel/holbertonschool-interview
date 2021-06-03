40524f2d4c5006e7612ded66235aa09a55346529
wassimbel <wassim.belhedi1@gmail.com>
2021-06-03 22:48:18 +0000

    update metrics f

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,9 +13,10 @@ def print_metrics(status_code, file_size):
             print("{}: {}".format(key, value))
 
 
-file_size = 0
-i = 0
-status_code = {"200": 0,
+def compute_metrics():
+    file_size = 0
+    i = 0
+    status_code = {"200": 0,
                "301": 0,
                "400": 0,
                "401": 0,
@@ -24,24 +25,28 @@ status_code = {"200": 0,
                "405": 0,
                "500": 0}
 
-try:
-    for j in sys.stdin:
-        metrics = j.split()
+    try:
+        for j in sys.stdin:
+            metrics = j.split()
 
-        if len(metrics) > 2:
-            i += 1
-            if i <= 10:
-                file_size += int(metrics[-1])
-                status = metrics[-2]
+            if len(metrics) > 2:
+                i += 1
+                if i <= 10:
+                    file_size += int(metrics[-1])
+                    status = metrics[-2]
 
-                if (status in status_code.keys()):
-                    status_code[status] += 1
+                    if (status in status_code.keys()):
+                        status_code[status] += 1
 
-            if (i == 10):
-                print_metrics(status_code, file_size)
-                i = 0
+                if (i == 10):
+                    print_metrics(status_code, file_size)
+                    i = 0
 
-except KeyboardInterrupt:
-    print_metrics(status_code, file_size)
-finally:
-    print_metrics(status_code, file_size)
+    except KeyboardInterrupt:
+        print_metrics(status_code, file_size)
+    finally:
+        print_metrics(status_code, file_size)
+
+
+if __name__ == '__main__':
+    compute_metrics()
