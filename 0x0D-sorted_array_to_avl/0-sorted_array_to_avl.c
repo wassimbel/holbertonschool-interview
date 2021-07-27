4d42b8c7039c25953375647948f098d90f094ca8
wassimbel <wassim.belhedi1@gmail.com>
2021-07-27 23:27:13 +0000

    fix betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * sorted_array_to_avl - build an AVL from an array
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ * Return: returns a pointer to the root of the AVL or null
+ **/
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
@@ -13,6 +19,15 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	root = sort(NULL, array, 0, size - 1);
 	return (root);
 }
+
+/**
+ * sort - uses recursion to build AVL
+ * @parent: pointer to parent node
+ * @array: pointer to first element of the array
+ * @start: start index of array
+ * @end: end index of array
+ * Return: returns a pointer to the root node or null on failure
+ **/
 
 avl_t *sort(avl_t *parent, int *array, int start, int end)
 {
@@ -31,10 +46,18 @@ avl_t *sort(avl_t *parent, int *array, int start, int end)
 	return (root);
 }
 
+/**
+ * new_node - adds a new node
+ * @parent: node's parent
+ * @n: int value of node
+ * Return: returns a pointer to the created node or null on failure
+ **/
+
 avl_t *new_node(avl_t *parent, int n)
 {
 	avl_t *new_node;
-	new_node = (avl_t*)malloc(sizeof(avl_t));
+
+	new_node = (avl_t *)malloc(sizeof(avl_t));
 
 	if (!new_node)
 		return (NULL);
