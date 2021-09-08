0304674a37af4d46cdf865e0b286bf57039ba563
wassimbel <wassim.belhedi1@gmail.com>
2021-09-08 19:17:06 +0000

    count

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,8 +3,8 @@
 import requests
 
 
-def recursive_search(subreddit, word_list, titles, after=""):
-    """ecursive function that queries the Reddit API,
+def recursive_search(subreddit, word_list, titles={}, after=""):
+    """recursive function that queries the Reddit API,
        parses the title of all hot articles, and prints a sorted count
        of given keywords (case-insensitive, delimited by spaces """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
@@ -13,6 +13,11 @@ def recursive_search(subreddit, word_list, titles, after=""):
     request = requests.get(url,
                            headers=header,
                            allow_redirects=False)
+    if not after:
+        word_list = list(set(word_list))
+        for k, v in enumerate(word_list):
+            word_list[k] = k.lower()
+
     if request.status_code != 200:
         return None
     if after is None:
@@ -20,13 +25,13 @@ def recursive_search(subreddit, word_list, titles, after=""):
 
     for i in request.json().get('data').get('children'):
         title = i.get('data').get('title').split()
-        for j in set(word_list):
-            if j.lower() in [k.lower() for k in title]:
-                if titles.get(j):
-                    titles[j] += 1
-                else:
-                    titles[j] = 1
-
+        for j in word_list:
+            for k in title:
+                if j == k:
+                    if j not in titles:
+                        titles[j] = 1
+                    else:
+                        titles[j] += 1
     after = request.json().get('data').get('after')
     recursive_search(subreddit, word_list, titles, after)
     return titles
