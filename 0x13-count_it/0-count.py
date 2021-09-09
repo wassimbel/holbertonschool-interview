59e2b2210b90b6e01711212bd817f5693e48d60f
wassimbel <wassim.belhedi1@gmail.com>
2021-09-09 21:58:55 +0000

    fix

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """ Get request """
-import requests
 
 
-def recursive_search(subreddit, word_list, titles={}, after=""):
-    """recursive function that queries the Reddit API,
+def recursive_search(subreddit, word_list, titles, after=""):
+    """ecursive function that queries the Reddit API,
        parses the title of all hot articles, and prints a sorted count
        of given keywords (case-insensitive, delimited by spaces """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
                                                                  after)
     header = {'User-Agent': 'Reddit API'}
+    params = {'limit': 200, 'after': after}
     request = requests.get(url,
                            headers=header,
-                           allow_redirects=False)
-    if not after:
-        word_list = list(set(word_list))
-        for k, v in enumerate(word_list):
-            word_list[k] = k.lower()
-
+                           allow_redirects=False
+                           params=params)
     if request.status_code != 200:
         return None
     if after is None:
@@ -25,13 +21,13 @@ def recursive_search(subreddit, word_list, titles={}, after=""):
 
     for i in request.json().get('data').get('children'):
         title = i.get('data').get('title').split()
-        for j in word_list:
-            for k in title:
-                if j == k:
-                    if j not in titles:
-                        titles[j] = 1
-                    else:
-                        titles[j] += 1
+        for j in set(word_list):
+            if j.lower() in [k.lower() for k in title]:
+                if titles.get(j):
+                    titles[j] += 1
+                else:
+                    titles[j] = 1
+
     after = request.json().get('data').get('after')
     recursive_search(subreddit, word_list, titles, after)
     return titles
