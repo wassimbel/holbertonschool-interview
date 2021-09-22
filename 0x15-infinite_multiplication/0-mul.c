cc2e644742c41d11ab50fb85e20f5b5eae1fade5
wassimbel <wassim.belhedi1@gmail.com>
2021-09-22 23:52:41 +0000

    infinite multiplication

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * check_error - checks errors
+ * @argc: number of args
+ * @argv: double pointer to array of argv
+ * Return: returns 0 on success otherwise exits with status 98
+ */
+
+int check_error(int argc, char **argv)
+{
+	size_t k;
+	int i, j;
+	char *err = "Error\n";
+
+	if (argc != 3)
+	{
+		for (k = 0; k < strlen(err); k++)
+			_putchar(err[k]);
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				for (k = 0; k < strlen(err); k++)
+					_putchar(err[k]);
+				exit(98);
+			}
+		}
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: double pointer to array of argv
+ * Return: returns 0 on success otherwise 1
+ */
+
+int main(int argc, char **argv)
+{
+	int len1, len2, i, j, k, x, carry;
+	int *num;
+
+	check_error(argc, argv);
+	len1 = strlen(argv[1]);
+	len2 = strlen(argv[2]);
+	if (argv[1][0] == '0' || argv[2][0] == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (1);
+	}
+	num = calloc(len1 + len2, sizeof(*num));
+	if (!num)
+		return (1);
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		k = argv[1][i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			x = argv[2][j] - '0';
+			carry += num[i + j + 1] + (k * x);
+			num[j + i + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			num[i + j + 1] += carry;
+	}
+	k = num[0];
+	if (k == 0)
+		k = 1;
+	for (; k < len1 + len2; k++)
+		printf("%d", num[k]);
+	printf("\n");
+	free(num);
+	return (0);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,7 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+int check_error(int argc, char **argv);
+
+#endif /* _HOLBERTON_H_ */
