978758e01ad829722fada243339a7d10cdb17142
wassimbel <wassim.belhedi1@gmail.com>
2021-09-22 23:59:55 +0000

    fix

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -1,5 +1,10 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int check_error(int argc, char **argv);
