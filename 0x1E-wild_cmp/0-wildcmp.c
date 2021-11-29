0bfe5c67acdeda0e84169d3cd52a011348e201c6
wassimbel <wassim.belhedi1@gmail.com>
2021-11-29 01:06:58 +0000

    wild compare

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings and returns 1
+ * if the strings can be considered identical, otherwise return 0
+ * @s1: string1
+ * @s2: string2 can contain the special character *
+ * Return: returns 1 if the strings can be considered identical,
+ * otherwise return 0
+ **/
+int wildcmp(char *s1, char *s2)
+{
+
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
